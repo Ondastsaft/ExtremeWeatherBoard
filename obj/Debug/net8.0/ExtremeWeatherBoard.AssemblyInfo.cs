1c6255ddf49f16a3a33fa04e174376cb1742ddcb
Ondastsaft <kenneth.jansson@gmail.com>
2024-05-30 12:06:42 +0200

    assemblyinfo

--- obj/Debug/net8.0/ExtremeWeatherBoard.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExtremeWeatherBoard.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExtremeWeatherBoard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8553f31f52bd1d42a293dbf806d64faa26ae3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f304f18c376da5a49e064cabc30093e0633f2fff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExtremeWeatherBoard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExtremeWeatherBoard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
