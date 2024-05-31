bf753d9b5905293530f91d9a587961acc6ab1830
Ondastsaft <kenneth.jansson@gmail.com>
2024-05-31 20:16:31 +0200

    Added Interfaces, services, DAL, page renders navigation properties from database in sidebar
    Lots of changes. as usual.

---
+++ Interfaces/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtremeWeatherBoard.Interfaces
+{
+    using ExtremeWeatherBoard.Models;
+    public interface ICategoryService
+    {
+        //Task<IEnumerable<Category>> GetCategoriesAsync();
+        //Task<Category> GetCategoryAsync(int id);
+
+    }
+}
---
+++ Pages/SubCategories.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExtremeWeatherBoard.Pages
+{
+    public class SubCategoriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
