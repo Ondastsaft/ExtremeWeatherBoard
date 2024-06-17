54046edebd3ca472bdbac02d5f5cc5138179ced7
Ondastsaft <kenneth.jansson@gmail.com>
2024-06-17 12:31:42 +0200

    Changed AdminUserData
    Now only contains specific admin properties and will be connected to identity user alongside ordinary UserData

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -19,41 +19,22 @@ namespace ExtremeWeatherBoard.Data
                    .HasMany(u => u.ReceivedMessages)
                    .WithOne(m => m.Receiver)
                    .OnDelete(DeleteBehavior.ClientSetNull);
-            modelBuilder.Entity<AdminUserData>()
-                   .HasMany(a => a.ReceivedMessages)
-                   .WithOne(m => m.AdminReceiver)
-                   .OnDelete(DeleteBehavior.ClientSetNull);
             modelBuilder.Entity<UserData>()
                 .HasMany(u => u.SentMessages)
                 .WithOne(m => m.Sender)
-                .OnDelete(DeleteBehavior.NoAction);
-            modelBuilder.Entity<AdminUserData>()
-                .HasMany(a => a.SentMessages)
-                .WithOne(m => m.AdminSender)
                 .OnDelete(DeleteBehavior.NoAction);
             modelBuilder.Entity<Message>()
                 .HasOne(m => m.Sender)
                 .WithMany(u => u.SentMessages)
                 .OnDelete(DeleteBehavior.NoAction);
             modelBuilder.Entity<Message>()
-                .HasOne(m => m.AdminSender)
-                .WithMany(a => a.SentMessages)
-                .OnDelete(DeleteBehavior.NoAction);
-            modelBuilder.Entity<Message>()
                 .HasOne(m => m.Receiver)
                 .WithMany(u => u.ReceivedMessages)
                 .OnDelete(DeleteBehavior.ClientSetNull);
-            modelBuilder.Entity<Message>()
-                .HasOne(m => m.AdminReceiver)
-                .WithMany(a => a.ReceivedMessages)
-                .OnDelete(DeleteBehavior.ClientSetNull);
-
             modelBuilder.Entity<Category>()
                 .HasOne(c => c.CreatorAdminUser)
                 .WithMany(au => au.Categories)
                 .OnDelete(DeleteBehavior.ClientSetNull);
-
-
             modelBuilder.Entity<SubCategory>()
                 .HasOne(s => s.SubCategoryAdminUserData)
                 .WithMany(au => au.SubCategories)
@@ -66,25 +47,13 @@ namespace ExtremeWeatherBoard.Data
                    .HasMany(a => a.SubCategories)
                    .WithOne(s => s.SubCategoryAdminUserData)
                    .OnDelete(DeleteBehavior.NoAction);
-            modelBuilder.Entity<AdminUserData>()
-                    .HasMany(a => a.DiscussionThreads)
-                    .WithOne(a => a.DiscussionThreadAdminUserData)
-                    .OnDelete(DeleteBehavior.NoAction);
             modelBuilder.Entity<DiscussionThread>()
                     .HasMany(d => d.Comments)
                     .WithOne(c => c.CommentThread)
                     .OnDelete(DeleteBehavior.NoAction);
-            modelBuilder.Entity<AdminUserData>()
-                    .HasMany(a => a.Comments)
-                    .WithOne(c => c.CommentAdminUserData)
-                    .OnDelete(DeleteBehavior.NoAction);
             modelBuilder.Entity<Comment>()
                     .HasOne(c => c.CommentThread)
                     .WithMany(dt => dt.Comments)
-                    .OnDelete(DeleteBehavior.NoAction);
-            modelBuilder.Entity<Comment>()
-                    .HasOne(c => c.CommentAdminUserData)
-                    .WithMany(u => u.Comments)
                     .OnDelete(DeleteBehavior.NoAction);
         }
 
--- Models/AdminUserData.cs
+++ Models/AdminUserData.cs
@@ -5,22 +5,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExtremeWeatherBoard.Models
 {
-    public class AdminUserData : IUser
+    public class AdminUserData
     {
-        public int Id { get; set; }
-        public string? ImageURL { get; set; }
+        public int Id { get; set; }        
         public string? UserId { get; set; }
         [ForeignKey("UserId")]
         public virtual IdentityUser? User { get; set; }
-        [InverseProperty("DiscussionThreadAdminUserData")]
-        public virtual ICollection<DiscussionThread>? DiscussionThreads { get; set; }
-        [InverseProperty("CommentAdminUserData")]
-        public virtual ICollection<Comment>? Comments { get; set; }
-        [InverseProperty("AdminReceiver")]
-        public virtual ICollection<Message>? ReceivedMessages { get; set; }
-        [InverseProperty("AdminSender")]
-        public virtual ICollection<Message>? SentMessages { get; set; }
-        [InverseProperty("CreatorAdminUser")]
         public virtual ICollection<Category>? Categories { get; set; }
         [InverseProperty("SubCategoryAdminUserData")]
         public virtual ICollection<SubCategory>? SubCategories { get; set; }
